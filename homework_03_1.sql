1b5f2d078accbcb56c13a780da623542bb2695e9
Михаил_Л <muxa413@mail.ru>
2025-01-24 12:28:36 +0300

    end HW 3.1

--- homework_03_1.sql
+++ homework_03_1.sql
@@ -13,6 +13,26 @@ FROM orders
 JOIN customers USING (customer_id)
 JOIN employees USING (employee_id)
 JOIN shippers ON orders.ship_via = shippers.shipper_id
-WHERE customers.city = 'London' AND employees.city = 'London' AND shippers.company_name = 'Speedy Express'
+WHERE customers.city = 'London' AND employees.city = 'London' AND shippers.company_name = 'Speedy Express';
 
 -- 2
+SELECT product_name, units_in_stock, contact_name, phone
+FROM products
+JOIN categories USING (category_id)
+JOIN suppliers USING (supplier_id)
+WHERE discontinued = 0 AND category_name IN ('Beverages','Seafood') AND units_in_stock < 20
+ORDER BY units_in_stock;
+
+-- 3
+SELECT contact_name, order_id
+FROM customers
+LEFT JOIN orders USING (customer_id)
+WHERE order_id IS NULL
+ORDER BY contact_name;
+
+-- 4
+SELECT contact_name, order_id
+FROM orders
+RIGHT JOIN customers USING (customer_id)
+WHERE order_id IS NULL
+ORDER BY contact_name;
